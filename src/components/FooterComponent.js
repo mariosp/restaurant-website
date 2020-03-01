fd74d8f116f759ffeafd4e311b21de1e20b1c9f4
Marios <mariospatsis@gmail.com>
2020-03-01 20:54:02 +0200

    assignment 2

--- src/components/FooterComponent.js
+++ src/components/FooterComponent.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import {Link} from "react-router-dom";
 
 function Footer(props) {
     return(
@@ -8,10 +9,10 @@ function Footer(props) {
                     <div className="col-4 offset-1 col-sm-2">
                         <h5>Links</h5>
                         <ul className="list-unstyled">
-                            <li><a href="#">Home</a></li>
-                            <li><a href="#">About</a></li>
-                            <li><a href="#">Menu</a></li>
-                            <li><a href="contactus.html">Contact</a></li>
+                            <Link to="/home"><li>Home</li></Link>
+                            <Link to="/aboutus"><li>About us</li></Link>
+                            <Link to="/menu"><li>Menu</li></Link>
+                            <Link to="/contactus"><li>Contact us</li></Link>
                         </ul>
                     </div>
                     <div className="col-7 col-sm-5">
