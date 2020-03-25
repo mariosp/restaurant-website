552771a90a0ad29713950e4af6f878c6a19ee32c
Marios <mariospatsis@gmail.com>
2020-03-25 16:33:47 +0200

    node file

---
+++ json-server/index.js
@@ -0,0 +1,21 @@
+const path = require("path");
+const express = require("express");
+const jsonServer = require('json-server');
+const server = jsonServer.create();
+const router = jsonServer.router('db.json');
+const middlewares = jsonServer.defaults();
+const publicPath = path.join(__dirname, '..', 'build');
+const port = process.env.PORT || 3000;
+
+server.use(middlewares);
+server.use('/static',express.static(publicPath));
+server.use(router);
+
+// Avoid CORS issue
+server.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
+server.listen(port);
